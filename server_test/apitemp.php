50f2721cf1edc017d805a7ca62aefd4d61402baf
Rafel Socarras <rafael.socarras@gmail.com>
2014-05-17 13:31:38 -0500

    cambios y comentarios del archivo

--- server_test/apitemp.php
+++ server_test/apitemp.php
@@ -1,22 +1,28 @@
 <?php
+    /*
+        Este archivo lee el archivo de CSV en donde estan los datos
+        de prueba a mostrar, lo que hace es mostrar la primra linea
+        del archivo la guarda en una variable, esta la compara para 
+        guardar el nuevo archivo sin la linea mostrada.
 
+    */
 
-     $data = file("datos.csv");
-     $out = array();
-     echo $DELETE= trim($data[0]);
+     $datos = file("datos.csv");
+     $salida = array();
+     echo $a_borrar= trim($datos[0]);
  
-     foreach($data as $line) {
+     foreach($datos as $linea) {
      	
-         if(trim($line) != $DELETE) {
-             $out[] = $line;
+         if(trim($linea) != $a_borrar) {
+             $salida[] = $linea;
          }
        
      }
 
      $fp = fopen("datos.csv", "w+");
      flock($fp, LOCK_EX);
-     foreach($out as $line) {
-         fwrite($fp, $line);
+     foreach($salida as $linea) {
+         fwrite($fp, $linea);
      }
      flock($fp, LOCK_UN);
      fclose($fp);  
