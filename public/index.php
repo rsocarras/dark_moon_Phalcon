cd3f93e057751cbc8000c50e6b458bb13ccf204f
Eduar Carvajal <carvajaldiazeduar@gmail.com>
2014-05-17 13:58:14 -0500

    agree .htaccess

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL);
 
-use Phalcon\Mvc\Micro as App;
+use Phalcon\Mvc\Micro as App; 
 
 define ('APP_PATH', realpath('../app'));
 
@@ -13,7 +13,7 @@ try {
 //
 
 	//Variable sd configuracion
-	$url = "http://10.0.0.121/apitemp_linea.php";
+	$url = "http://10.0.0.121/dark_moon_Phalcon/server_test/apitemp.php";
 	$separador = ";";
  
 	$app = new App();
@@ -51,6 +51,22 @@ try {
 		$val = $client->lrange('last', -1, -1);
 		echo $val[0];
 	});
+
+	/**
+	 * Return a coordinates by a file initial
+	 * $param string $type Example: file, rest   
+	 */
+	$app->get('/randCoordinate', function() use ($url) {
+		// Prepend a base path if Predis is not available in your "include_path".
+		require '../app/library/Predis/autoload.php';
+		Predis\Autoloader::register();
+
+		$client = new Predis\Client();
+
+		$val = $client->lrange('last', rand(1,1000)*-1, -1);
+		echo $val[0];
+	});	
+
 	$app->handle();
 
 } catch (Exception $e) {
